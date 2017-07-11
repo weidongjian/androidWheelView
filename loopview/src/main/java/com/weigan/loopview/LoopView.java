9797db1a9783c2cd44fc5f10d932294993de567c
laileon <leon@igola.com>
2017-07-11 11:35:37 +0800

    delete log

--- loopview/src/main/java/com/weigan/loopview/LoopView.java
+++ loopview/src/main/java/com/weigan/loopview/LoopView.java
@@ -15,7 +15,6 @@ import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.os.Handler;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
